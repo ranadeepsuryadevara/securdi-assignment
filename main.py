a9287b5bb9be8137689cba68b729544f0beedbf5
ranadeepsuryadevara <84175151+ranadeepsuryadevara@users.noreply.github.com>
2022-04-06 10:34:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
 import os
+from pickle import TRUE
 import subprocess
 
-cp = "sudo apt install isc-dhcp-server"
-cmd = cp
+codepath = " sudo apt-get update -y && sudo apt install isc-dhcp-server"
+cmd = codepath
 os.system(cmd)
 
-cp = "cp /securdi-assignment/dhcp.conf /etc/dhcp"
-cmd = cp
+codepath = " sudo mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.backup"
+cmd = codepath
 os.system(cmd)
 
-cp = "cp /securdi-assignment/isc-dhcp-server /etc/default"
-cmd = cp
+codepath = " cd /etc/dhcp"
+cmd = codepath
+os.system(cmd)
+file1 = open("dhcpd.conf", "a")  # append mode
+file1.write("""# a simple /etc/dhcp/dhcpd.conf
+default-lease-time 600;
+max-lease-time 7200;
+authoritative;
+ 
+subnet 192.168.1.0 netmask 255.255.255.0 {
+ range 192.168.1.100 192.168.1.200;
+ option routers 192.168.1.254;
+ option domain-name-servers 192.168.1.1, 192.168.1.2;
+#option domain-name "mydomain.example";
+}
+ \n""")
+file1.close()
+
+codepath = "cd /etc/default"
+cmd = codepath
+os.system(cmd)
+file1 = open("isc-dhcp-server", "a")  # append mode
+file1.write("INTERFACESv4="eth0" \n")
+file1.close()
+
+
+codepath = " sudo systemctl restart isc-dhcp-server.service"
+cmd = codepath
 os.system(cmd)
 
-cp = "sudo systemctl restart isc-dhcp-server.service"
-cmd = cp
+codepath = " sudo systemctl status isc-dhcp-server.service"
+cmd = codepath
 os.system(cmd)
 
-cp = "sudo systemctl status isc-dhcp-server.service"
-cmd = cp
-os.system(cmd)
+print("DHCP Has been successfully installed and Configured")
+
+
